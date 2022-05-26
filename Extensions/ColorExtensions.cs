e2e8f926995e5dc282ea572a9b8d156f0587f1e0
Daniel Artmann <daniel@artmann.co>
2022-05-26 16:44:30 +0200

    Fix: Changed the HexToColor implementation

--- Extensions/ColorExtensions.cs
+++ Extensions/ColorExtensions.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;
+﻿using System.Globalization;
+using UnityEngine;
 
 namespace Utility.Extensions
 {
@@ -7,7 +8,38 @@ namespace Utility.Extensions
         // input example: #E7AC54
         public static Color FromHexString(string hex)
         {
-            return ColorUtility.TryParseHtmlString(hex, out var c) ? c : default;
+            var c = (Color) ToColor(hex);
+            return c;
+            // return ColorUtility.TryParseHtmlString(hex, out var c) ? c : default;
+        }
+
+        // https://answers.unity.com/questions/812240/convert-hex-int-to-colorcolor32.html
+        private static Color32 ToColor(string hex)
+        {
+            if (string.IsNullOrEmpty(hex))
+            {
+                return default;
+            }
+
+            if (hex.StartsWith("#"))
+            {
+                hex = hex[1..];
+            }
+            
+            //assume fully visible unless specified in hex
+            byte a = 255; 
+            var numberStyles = NumberStyles.HexNumber;
+            byte r = byte.Parse(hex.Substring(0, 2), numberStyles);
+            byte g = byte.Parse(hex.Substring(2, 2), numberStyles);
+            byte b = byte.Parse(hex.Substring(4, 2), numberStyles);
+            
+            //Only use alpha if the string has enough characters
+            if (hex.Length == 8)
+            {
+                a = byte.Parse(hex.Substring(6, 2), numberStyles);
+            }
+
+            return new Color32(r, g, b, a);
         }
     }
 }
